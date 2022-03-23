ba04bc0fdf769e76a7324b61ffff4cb587204b08
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-03-23 01:29:22 -0700

    initial commit

---
+++ DiscordBot.py
@@ -0,0 +1,25 @@
+import discord
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+KEY = os.getenv('DISCORD_KEY')
+
+#logging set up
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(f'{KEY}')
